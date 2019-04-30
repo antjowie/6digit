1a8e3f1ba8cb29fb10187d835e74a1d27a1b3dde
Angelo <7332321+antjowie@users.noreply.github.com>
2019-04-30 20:29:20 +0200

    Can now curse people

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const config = require("./config.json");
+const { prefix, token } = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -10,12 +10,36 @@ client.once("ready", () => {
     console.log("Bot is online! Stop excecution with ctrl+c");
 });
 
+const cursedUsers = new Set();
 client.on("message", (message) => {
-    if (message.author.id === config.id) {
-        console.log(message.author.username + " tried to write: " + message.content);
-        message.delete();
-        message.channel.send("You have no permission to talk because you lost from a 6 digit");
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "curse") {
+        const user = message.mentions.users.first();
+        if (cursedUsers.has(user)) {
+            message.reply(`uncursed ${user.name}`);
+            cursedUsers.delete(user);
+        }
+        else {
+            message.reply(`cursed ${user.name}`);
+            cursedUsers.add(user);
+        }
     }
+    else if (command === "cursed") {
+        if (cursedUsers.size === 0) {
+            message.reply("no cursed users");
+        }
+        else {
+            message.reply(cursedUsers);
+        }
+    }
+    else {
+        message.reply("incorrect command");
+    }
+
 });
 
-client.login(config.token);
+client.login(token);
